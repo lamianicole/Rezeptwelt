4293c31497495896cabff43779264fee563bb1c1
Lamia Rizk <lamiarizk@gmx.de>
2025-01-26 16:54:50 +0100

    changed newCard

--- src/components/newRecipeCard/NewRecipeCard.tsx
+++ src/components/newRecipeCard/NewRecipeCard.tsx
@@ -8,7 +8,7 @@ interface INewRecipeCardProps{
 const NewRecipeCard:React.FC<INewRecipeCardProps> = ({newRecipe}) => {
 return ( 
     <article className="flex items-center bg-slate-100 rounded-xl shadow-lg overflow-hidden h-48 w-3/4 mx-auto">
-        <img className="h-full w-48 object-cover" src="/LogoMug.png" alt="Food image" />
+        <img className="h-full w-48 object-cover" src={`${newRecipe.image_url}`} alt="Food image" />
         <div className="p-8 text-start bg-slate-100 h-full w-full">
             <h3 className="text-xl font-semibold pb-2">{newRecipe.name}</h3>
             <p className="pb-4 text-xs">{newRecipe.description}</p>
